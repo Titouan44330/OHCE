9757392f02bd0ecd0af897ec9d7c9989806fd387
TitouanLC <84964261+Titouan44330@users.noreply.github.com>
2023-01-31 23:34:42 +0100

    Etape 4
    peaufinages de la gestion de la langue

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ if langue_systeme == "fr":
     langue = "Français"
 elif langue_systeme == "en_GB":
     langue = "Anglais"
+else :
+    langue = "NONE"
 
 if __name__ == '__main__':
     print("Bonjour !")
 
     while (1):
         text = input('Saisissez un texte : ')
-        if (text == 'quitter' or text == 'quit'):
+        if (text == 'quitter' or text == 'quit' or langue == "NONE"):
+            if langue == "NONE":
+                print("La langue de votre système n'est pas géré par le logiciel !")
             print("Au revoir !")
             break
         print(Ohce.miroir(locale, text, langue, now.hour))
